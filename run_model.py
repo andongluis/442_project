0bddefa9e8a31b0b304113e0c929f1f1b0d42e63
andongluis <aluisli@umich.edu>
2019-04-16 21:25:37 -0400

    a couple more changes

--- run_model.py
+++ run_model.py
@@ -4,52 +4,66 @@ Should be able to just call this function and set some flags
 '''
 from model import models
 from model.utility_nn import nn_train, nn_stats
-from processing import Preprocessor, Dataset
-from utility.graphing import plot_history
+from processing.preprocessor import Preprocessor
+from processing.dataset import Dataset
+from utility.functions import plot_history, process_raw_csv
 import sys
 import yaml
-
-# Read flags
-if len(sys.argv)> 1:
-    args = [sys.argv[1] ]
-    if len(sys.argv) > 2:
-    	args.append(sys.argv[2])
-    flags = {}
-    if "images" in args:
-    	flags["images"] = True
-    if "fc" in args:
-    	flags["fc"] = True
-
-    if len(flags.keys()) == 0:
-    	print("Error: no valid flags")
-    	sys.exit(1)
-else:
-    print("Error: need to specify flags for type of input")
-    sys.exit(1)
+from sklearn.model_selection import train_test_split
 
 
-# Read config files
+def main():
+    # Read flags
+    if len(sys.argv) > 1:
+        args = [sys.argv[1] ]
+        if len(sys.argv) > 2:
+        	args.append(sys.argv[2])
+        flags = {}
+        if "images" in args:
+        	flags["images"] = True
+        if "fc" in args:
+        	flags["fc"] = True
+
+        if len(flags.keys()) == 0:
+        	print("Error: no valid flags")
+        	sys.exit(1)
+    else:
+        print("Error: need to specify flags for type of input")
+        sys.exit(1)
 
 
-# Read data and preprocess it
-# Have ready train, valid, test
-trainX, trainY
-validX, validY
-testX, testY
-
-Preprocessor()
+    # Read config files
 
 
-# Build models
-model = models.cnn_model(trainX.shape)
+    # Read data and have ready train, valid, test
+    df = process_raw_csv("batch_1.csv")
+    X = df.drop("likes", axis=1)
+    Y = df["likes"]
+    trainX, testX, trainY, testY = train_test_split(X,Y, test_size=0.25)
+    trainX, validX, trainY, validY = train_test_split(trainX, trainY, test_size=0.25)
 
-# Train models
-model, adam_hist, sgd_hist = nn_train(model, trainX, trainY, validX, validY, multi_input=False)
+    # Preprocess data
+    processor =  Preprocessor(flags, config_file="./config/preprocess.yml")
+    train_D = processor.fit(trainX)
+    valid_D = processor.transform(validX)
+    test_D = processor.transform(testX)
 
-# Give results
-nn_stats(model, trainX, trainY, multi_input=False, name="Train")
-nn_stats(model, validX, validY, multi_input=False, name="Valid")
-nn_stats(model, testX,  testY,  multi_input=False, name="Test")
 
-# Plot stuff
-plot_history(adam_hist, sgd_hist, path="./", acc=True, loss=True)
+
+    # Build models
+    model = models.cnn_model(trainX.shape)
+
+    # Train models
+    model, adam_hist, sgd_hist = nn_train(model, trainX, trainY, validX, validY, multi_input=False)
+
+    # Give results
+    nn_stats(model, trainX, trainY, multi_input=False, name="Train")
+    nn_stats(model, validX, validY, multi_input=False, name="Valid")
+    nn_stats(model, testX,  testY,  multi_input=False, name="Test")
+
+    # Plot stuff
+    plot_history(adam_hist, sgd_hist, path="./", acc=True, loss=True)
+
+
+if __name__ == "__main__":
+    main()
