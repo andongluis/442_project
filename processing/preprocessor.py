8ffccaeff621e5c85806170efd6e3cdd164a0716
andongluis <aluisli@umich.edu>
2019-04-20 13:32:31 -0400

    added normalization, some stuff on images

--- processing/preprocessor.py
+++ processing/preprocessor.py
@@ -1,8 +1,12 @@
 '''
 This will be a class that handles the preprocessing of data.
 This class will preprocess data
+Note: This will only handle features, assumes labels are already removed from df
 '''
 from .dataset import Dataset
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from PIL import Image
+
 
 import yaml
 
@@ -53,14 +57,36 @@ class Preprocessor:
         if self.flags["images"]:
 
             # Process images
+            self.max_width = 0
+            self.max_height = 0
+            X = []
+            for filename in df["name"]:
+                img = load_img('data/train/cats/cat.0.jpg')  # this is a PIL image
+                if img.size[2] > self.max_height:
+                    self.max_height = img.size[2]
+                if img.size[1] > self.max_width:
+                    self.max_width = img.size[1]               
+                X.append(img)
+
+            # Make all images the same size
+            for idx, img in enumerate(X):
+                img = img = img.resize((self.max_width, self.max_height), Image.ANTIALIAS)
+                X[idx] = img_to_array(img) # this is a Numpy array
+
 
             if self.downsize:
                 # Make images smaller
                 pass
 
+            X = np.asarray(X)
+
             if self.normalize:
                 # normalize images
-                pass
+                self.img_mean = np.mean(X, axis=(1,2), keepdims=True)
+                self.img_std = np.std(X, axis=(1,2), keepdims=True)
+                X = (X - self.img_mean) / self.img_std
+
+            dataset.set_images(X)
 
         if self.flags["fc"]:
 
@@ -70,11 +96,14 @@ class Preprocessor:
                 # Make NLP features
                 pass
 
+            fc_data = df.drop(self.drop_cols, axis=1)
+
             if self.normalize:
                 # normalize stuff
-                pass
+                self.fc_mean = df.mean()
+                self.fc_std = df.std()
+                df = (self.fc_mean) / self.fc_std
 
-            fc_data = df.drop(self.drop_cols, axis=1)
             # fc_data = df[["followers"]]
 
             dataset.set_fc_data(fc_data)
@@ -99,14 +128,25 @@ class Preprocessor:
         if self.flags["images"]:
 
             # Process images
+            X = []
+            for filename in df["name"]:
+                img = load_img('data/train/cats/cat.0.jpg')  # this is a PIL image
+                img = img.resize((self.max_width, self.max_height), Image.ANTIALIAS)
+                x = img_to_array(img)  # this is a Numpy array
+                X.append(x)
+
 
             if self.downsize:
                 # Make images smaller
                 pass
 
+            X = np.asarray(X)
+
             if self.normalize:
                 # normalize images
-                pass
+                X = (X - self.img_mean) / self.img_std
+
+            dataset.set_images(X)
 
         if self.flags["fc"]:
 
@@ -116,11 +156,12 @@ class Preprocessor:
                 # Make NLP features
                 pass
 
+            fc_data = df.drop(self.drop_cols, axis=1)
+
             if self.normalize:
                 # normalize stuff
-                pass
+                df = (self.fc_mean) / self.fc_std
 
-            fc_data = df.drop(self.drop_cols, axis=1)
             # fc_data = df[["followers"]]
 
             dataset.set_fc_data(fc_data)
